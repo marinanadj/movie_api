d22fac4bc5d214741250396175969af4f0e8336a
marinanadj <marinanadj7@yahoo.com>
2022-05-19 23:06:03 +0200

    update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const { check, validationResult } = require('express-validator');
  
 // mongoose.connect('mongodb://localhost:27017/[myFlixDB]', { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connect(process.env.CONNECTION_URI || "mongodb+srv://marinkec7:@myflixdb.f1d5x.mongodb.net/myFlixDB?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://admin:Pass@022@myflixdb.f1d5x.mongodb.net/myFlixDB?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 const morgan = require('morgan'),
